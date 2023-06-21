baba78293b2d597ffaa260544149f9d382c014b7
oovillagran <oscar.villagran@hotmail.com>
2023-06-21 15:54:24 -0500

    Add DecodeMorseCodeMessage class

--- decode_morse_code.rb
+++ decode_morse_code.rb
@@ -1,19 +1,19 @@
 class DecodeMorseCodeMessage
   MORSE_CODE = {
-    '.-'    => 'A', '-...'  => 'B', '-.-.'  => 'C', '-..'   => 'D', '.'     => 'E',
-    '..-.'  => 'F', '--.'   => 'G', '....'  => 'H', '..'    => 'I', '.---'  => 'J',
-    '-.-'   => 'K', '.-..'  => 'L', '--'    => 'M', '-.'    => 'N', '---'   => 'O',
-    '.--.'  => 'P', '--.-'  => 'Q', '.-.'   => 'R', '...'   => 'S', '-'     => 'T',
-    '..-'   => 'U', '...-'  => 'V', '.--'   => 'W', '-..-'  => 'X', '-.--'  => 'Y',
-    '--..'  => 'Z'
-  }
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }.freeze
 
   def self.decode_char(morse_code)
     MORSE_CODE[morse_code] || ''
   end
 
   def self.decode_word(morse_word)
-    morse_word.split(' ').map { |morse_char| decode_char(morse_char) }.join('')
+    morse_word.split.map { |morse_char| decode_char(morse_char) }.join
   end
 
   def self.decode(message)
@@ -23,7 +23,6 @@ class DecodeMorseCodeMessage
   end
 end
 
-decoder = DecodeMorseCodeMessage.new
-decoded_message = decoder.decode_message("-- -.--   -. .- -- .")
+decoded_message = DecodeMorseCodeMessage.decode('-- -.--   -. .- -- .')
 
 puts decoded_message
